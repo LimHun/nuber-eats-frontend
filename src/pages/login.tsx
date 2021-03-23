ec27728e81dd94dc5157c901258a173dcd5a11cd
LimHun <dlagnskkld3@gmail.com>
2021-03-24 00:40:24 +0900

    15.9 complite

---
+++ src/pages/login.tsx
@@ -0,0 +1,96 @@
+import React from "react"
+import { useForm } from "react-hook-form";
+import { gql, useMutation } from "@apollo/client";
+import { FormError } from "../components/form-error";
+import { loginMutation, loginMutationVariables } from "../__generated__/loginMutation";
+
+
+const LOGIN_MUTATION = gql`
+    mutation loginMutation($loginInput: LoginInput!) {
+    login(input: $loginInput) {
+        ok
+        token
+        error
+    }
+  }
+`;
+
+interface ILoginForm {
+    email: string;
+    password: string;
+    resultError? : string;
+}
+
+export const Login = () => { 
+    const { register, getValues, errors, handleSubmit } = useForm<ILoginForm>();
+    const onCompleted = (data: loginMutation) => {
+        const {
+            login: {error, ok, token},
+        } = data;
+        if (ok) {
+            console.log(token);
+            console.log(error);
+        }
+    };
+    const [loginMutation, {data: loginMutationResult, loading}] = useMutation<
+    loginMutation,
+    loginMutationVariables
+  >(LOGIN_MUTATION, {onCompleted});
+
+    const onSubmit = () => {
+        if(!loading) {
+            const { email, password } = getValues();
+            loginMutation({
+            variables: {   
+                loginInput: {
+                    email,
+                    password,
+                },
+            }
+        }
+    )};
+    };
+    return (
+        <div className="h-screen flex items-center justify-center bg-gray-800">
+        <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
+            <h3 className="font-bold text-lg text-gray-800">Log In</h3>
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="grid gap-3 mt-5 px-5"
+            >
+            <input
+                ref={register({ required: "Email is required" })}
+                name="email"
+                required
+                type="email"
+                placeholder="Email"
+                className="input"
+            />
+            {errors.email?.message && (
+                <FormError errorMessage={errors.email?.message} />
+            )}
+            <input
+                ref={register({ required: "Password is required", minLength: 10 })}
+                required
+                name="password"
+                type="password"
+                placeholder="Password"
+                className="input"
+            />
+            {errors.password?.message && (
+                <FormError errorMessage={errors.password?.message} />
+            )}
+            {errors.password?.type === "minLength" && (
+                <FormError errorMessage="Password must be more than 10 chars." />
+            )}
+            <button className="mt-3 btn">
+                {loading ? "Loading..." : "Log In"}
+            </button>
+            {loginMutationResult?.login.error && (
+            <FormError errorMessage={loginMutationResult.login.error} />
+            )}
+            </form>
+        </div>
+        </div>
+    );
+}
---
+++ src/routers/logged-out-router.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CreateAccount } from "../pages/create-account";
+import { Login } from "../pages/login";
+ 
+
+export const LoggedOutRouter = () => {
+    return (
+    <Router>
+        <Switch>
+            <Route path="/create-account">
+                <CreateAccount />
+            </Route>
+            <Route path="/">
+                <Login />
+            </Route>
+        </Switch>
+    </Router>
+    );
+ };
